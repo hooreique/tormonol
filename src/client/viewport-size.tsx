8860784a51628c5931bfe7d2917f18ad90a23bd2
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-12-13 09:36:17 +0900

    add jsdoc for viewport-size.tsx

--- src/client/viewport-size.tsx
+++ src/client/viewport-size.tsx
@@ -1,25 +1,27 @@
-import { ComponentChildren, createContext } from 'preact';
+import type { ComponentChildren } from 'preact';
+import { createContext } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
 
+
+/**
+ * 현재 뷰포트의 너비가 큰지 작은지 알려주는 Context
+ */
 export const ViewportWidthContext = createContext<{
+  /**
+   * 뷰포트의 너비가 960px 미만이면 `true`, 그렇지 않으면 `false`
+   */
   readonly isSmall: boolean;
-}>(undefined);
+}>({ isSmall: false });
 
 export const ViewportWidthContextProvider = ({ children }: { children: ComponentChildren }) => {
-  const mql = matchMedia("(width < 960px)");
+  const mql: MediaQueryList = matchMedia("(width < 960px)");
 
   const [isSmall, setIsSmall] = useState<boolean>(mql.matches);
 
   useEffect(() => {
-    const listener = (ev: MediaQueryListEvent) => {
-      setIsSmall(ev.matches);
-    };
-
+    const listener = (ev: MediaQueryListEvent): void => setIsSmall(ev.matches);
     mql.addEventListener('change', listener);
-
-    return () => {
-      mql.removeEventListener('change', listener);
-    };
+    return () => mql.removeEventListener('change', listener);
   });
 
   return <ViewportWidthContext.Provider value={{ isSmall }}>
